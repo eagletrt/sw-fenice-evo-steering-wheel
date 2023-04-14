4c04c493a5781e89aeff56e0d801b93d99ebc259
tzarjakob <giacomo.mazzucchi@protonmail.com>
2023-04-14 13:45:23 +0200

    chore: removing unused custom memory alloc functions

--- Core/Src/lvgl_port/custom_memory_alloc.c
+++
@@ -1,7 +0,0 @@
-#include "lvgl_port/custom_memory_alloc.h"
-
-void *steering_malloc(size_t size) { return NULL; }
-
-void *steering_realloc(void *ptr, size_t size) { return NULL; }
-
-void steering_free(void *ptr) {}
