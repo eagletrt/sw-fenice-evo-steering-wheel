8da579e91094bce9322bbbb08c7f452ebbc7a033
Lyreplus <lyreplus@gmail.com>
2024-05-29 18:40:09 +0200

    testing with lv_meter

--- Core/UI/steering/tab_manager.c
+++ Core/UI/steering/tab_manager.c
@@ -10,6 +10,14 @@ lv_obj_t *tab_hv_ptr;
 lv_obj_t *tab_lv_ptr;
 lv_obj_t *tab_test_ptr;
 
+static lv_obj_t * meter;
+
+static void set_value(void * indic, int32_t v)
+{
+    lv_meter_set_indicator_end_value(meter, indic, v);
+}
+
+
 #if STEER_TAB_CALIBRATION_ENABLED == 1
 lv_obj_t *tab_calibration_ptr;
 #endif
@@ -43,9 +51,7 @@ void load_current_racing_tab();
 void load_current_engineering_tab();
 
 void tab_manager(void) {
-    init_custom_styles();
-
-    
+    init_custom_styles();    
 
     //tab_racing_ptr  = lv_obj_create(NULL);
 
@@ -79,9 +85,9 @@ void tab_manager(void) {
                              "Can you see it?");
 
     /*Create a spinner*/
-    lv_obj_t * spinner = lv_spinner_create(tab1, 1000,60);
-    lv_obj_set_size(spinner, 100, 100);
-    lv_obj_center(spinner);
+    // lv_obj_t * spinner = lv_spinner_create(tab1, 1000,60);
+    // lv_obj_set_size(spinner, 100, 100);
+    // lv_obj_center(spinner);
 
     // label = lv_label_create(tab2);
     // lv_label_set_text(label, "Second tab");
@@ -89,7 +95,99 @@ void tab_manager(void) {
     // label = lv_label_create(tab3);
     // lv_label_set_text(label, "Third tab");
 
-    lv_obj_scroll_to_view_recursive(label, LV_ANIM_OFF);
+    // lv_obj_scroll_to_view_recursive(label, LV_ANIM_OFF);
+
+    // meter = lv_meter_create(lv_scr_act());
+    // lv_obj_center(meter);
+    // lv_obj_set_size(meter, 200, 200);
+
+    // /*Remove the circle from the middle*/
+    // lv_obj_remove_style(meter, NULL, LV_PART_INDICATOR);
+
+    // /*Add a scale first*/
+    // lv_meter_scale_t * scale = lv_meter_add_scale(meter);
+    // lv_meter_set_scale_ticks(meter, scale, 11, 2, 10, lv_palette_main(LV_PALETTE_GREY));
+    // lv_meter_set_scale_major_ticks(meter, scale, 1, 2, 30, lv_color_hex3(0xeee), 10);
+    // lv_meter_set_scale_range(meter, scale, 0, 100, 270, 90);
+
+    // /*Add a three arc indicator*/
+    // lv_meter_indicator_t * indic1 = lv_meter_add_arc(meter, scale, 10, lv_palette_main(LV_PALETTE_RED), 0);
+    // lv_meter_indicator_t * indic2 = lv_meter_add_arc(meter, scale, 10, lv_palette_main(LV_PALETTE_GREEN), -10);
+    // lv_meter_indicator_t * indic3 = lv_meter_add_arc(meter, scale, 10, lv_palette_main(LV_PALETTE_BLUE), -20);
+
+    // /*Create an animation to set the value*/
+    // lv_anim_t a;
+    // lv_anim_init(&a);
+    // lv_anim_set_exec_cb(&a, set_value);
+    // lv_anim_set_values(&a, 0, 100);
+    // lv_anim_set_repeat_delay(&a, 100);
+    // lv_anim_set_playback_delay(&a, 100);
+    // lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+
+    // lv_anim_set_time(&a, 2000);
+    // lv_anim_set_playback_time(&a, 500);
+    // lv_anim_set_var(&a, indic1);
+    // lv_anim_start(&a);
+
+    // lv_anim_set_time(&a, 1000);
+    // lv_anim_set_playback_time(&a, 1000);
+    // lv_anim_set_var(&a, indic2);
+    // lv_anim_start(&a);
+
+    // lv_anim_set_time(&a, 1000);
+    // lv_anim_set_playback_time(&a, 2000);
+    // lv_anim_set_var(&a, indic3);
+    // lv_anim_start(&a);
+    
+
+    meter = lv_meter_create(lv_scr_act());
+    lv_obj_center(meter);
+    lv_obj_set_size(meter, 200, 200);
+
+    /*Add a scale first*/
+    lv_meter_scale_t * scale = lv_meter_add_scale(meter);
+    lv_meter_set_scale_ticks(meter, scale, 41, 2, 10, lv_palette_main(LV_PALETTE_GREY));
+    lv_meter_set_scale_major_ticks(meter, scale, 8, 4, 15, lv_color_black(), 10);
+
+    lv_meter_indicator_t * indic;
+
+    /*Add a blue arc to the start*/
+    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_BLUE), 0);
+    lv_meter_set_indicator_start_value(meter, indic, 0);
+    lv_meter_set_indicator_end_value(meter, indic, 20);
+
+    /*Make the tick lines blue at the start of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_BLUE), false, 0);
+    lv_meter_set_indicator_start_value(meter, indic, 0);
+    lv_meter_set_indicator_end_value(meter, indic, 20);
+
+    /*Add a red arc to the end*/
+    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_meter_set_indicator_start_value(meter, indic, 80);
+    lv_meter_set_indicator_end_value(meter, indic, 100);
+
+    /*Make the tick lines red at the end of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED), false, 0);
+    lv_meter_set_indicator_start_value(meter, indic, 80);
+    lv_meter_set_indicator_end_value(meter, indic, 100);
+
+    /*Add a needle line indicator*/
+    indic = lv_meter_add_needle_line(meter, scale, 4, lv_palette_main(LV_PALETTE_GREY), -10);
+
+    /*Create an animation to set the value*/
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_exec_cb(&a, set_value);
+    lv_anim_set_var(&a, indic);
+    lv_anim_set_values(&a, 0, 100);
+    lv_anim_set_time(&a, 2000);
+    lv_anim_set_repeat_delay(&a, 100);
+    lv_anim_set_playback_time(&a, 500);
+    lv_anim_set_playback_delay(&a, 100);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_start(&a);
+
+
 
 //     tab_sensors_ptr = lv_obj_create(NULL);
 //     tab_hv_ptr      = lv_obj_create(NULL);
